e80086737e583d97b87b4ba1a5f0aa61b1a7745b
Ur-Village-People <urvillag3p3opl3@gmail.com>
2025-12-25 23:32:52 +0100

    push

--- app/js/script.js
+++ app/js/script.js
@@ -5,6 +5,13 @@ const yearInput = document.getElementById('years');
 const resYears = document.getElementById('res-years');
 const resMonths = document.getElementById('res-months');
 const resDays = document.getElementById('res-days');
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+document.onkeydown = function(e) {
+    if (e.key === "F12") return false;
+    if (e.ctrlKey && e.shiftKey && e.key == 'I'.charCodeAt(0)) return false;
+    if (e.ctrlKey && e.shiftKey && e.key == 'J'.charCodeAt(0)) return false;
+    if (e.ctrlKey && e.key == 'U'.charCodeAt(0)) return false;
+}; 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const day = parseInt(dayInput.value);
