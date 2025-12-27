22b753a1d35cc06bd5551be4740632ce918e52b6
Ur-Village-People <urvillag3p3opl3@gmail.com>
2025-12-27 14:40:03 +0100

    updating some styles

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,16 @@ const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
-// const babel = require('gulp-babel');
 const browsersync = require('browser-sync').create();
 const webpack = require('webpack-stream');
 const obfuscator = require('gulp-javascript-obfuscator');
-
-// Logic to check if we are in production mode
 const isProd = process.env.NODE_ENV === 'production';
-
 function copyTask() {
     return src(['*.html', 'assets/**/*'], { base: '.' })
         .pipe(dest('dist'));
 }
-
 function scssTask() {
-    return src('app/scss/style.scss', { sourcemaps: !isProd }) // Sourcemaps only in dev
+    return src('app/scss/style.scss', { sourcemaps: !isProd })
         .pipe(sass({
             includePaths: ['./app/scss/globals', './app/scss/components', './app/scss/util']
         }).on('error', sass.logError))
@@ -32,7 +27,6 @@ function jsTask() {
     let stream = src('app/js/script.js')
         .pipe(webpack({
             mode: isProd ? 'production' : 'development',
-            // CRUCIAL: 'eval-source-map' allows Console Ninja to find your code
             devtool: isProd ? false : 'eval-source-map',
             output: {
                 filename: 'script.js',
@@ -52,9 +46,6 @@ function jsTask() {
                 ]
             }
         }));
-
-    // Only obfuscate if we are in production mode
-    // Obfuscation breaks Console Ninja's ability to read variables
     if (isProd) {
         stream = stream.pipe(obfuscator({
             compact: true,
@@ -65,10 +56,8 @@ function jsTask() {
             stringArrayThreshold: 0.75
         }));
     }
-
     return stream.pipe(dest('dist'));
 }
-
 function browserSyncServe(cb) {
     browsersync.init({
         server: {
@@ -83,12 +72,10 @@ function browserSyncServe(cb) {
     });
     cb();
 }
-
 function browserSyncReload(cb) {
     browsersync.reload();
     cb();
 }
-
 function watchTask() {
     watch('*.html', series(copyTask, browserSyncReload));
     watch(
@@ -96,11 +83,7 @@ function watchTask() {
         series(scssTask, jsTask, browserSyncReload)
     );
 }
-
-// Default Task: Run this for development (Console Ninja will work here)
 exports.default = series(scssTask, jsTask, copyTask, browserSyncServe, watchTask);
-
-// Build Task: Run this for final deployment (Obfuscation will be active)
 exports.build = series(
     (cb) => { process.env.NODE_ENV = 'production'; cb(); }, 
     scssTask, 
